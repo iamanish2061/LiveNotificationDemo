0809f96c1b2e0497287a45428bf700f0f8009f14
AnishShrestha <anishnitro5@gmail.com>
2025-11-23 18:27:20 +0545

    services

---
+++ src/main/java/com/LiveNotificationDemo/service/OrderService.java
@@ -0,0 +1,90 @@
+package com.LiveNotificationDemo.service;
+
+import com.LiveNotificationDemo.dto.OrderEvent;
+import com.LiveNotificationDemo.entity.Order;
+import com.LiveNotificationDemo.repo.OrderRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepo orderRepo;
+    private final KafkaProducerService kafkaProducer;
+
+    public Order placeOrder(String productName, Double price, String customerUsername) {
+        Order order = Order.builder()
+                .productName(productName)
+                .price(price)
+                .customerUsername(customerUsername)
+                .status(Order.OrderStatus.PENDING)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        order = orderRepo.save(order);
+
+        // Send event to Kafka
+        OrderEvent event = new OrderEvent(
+                order.getId(),
+                customerUsername,
+                productName,
+                price,
+                order.getStatus(),
+                "New order placed!"
+        );
+        kafkaProducer.sendOrderEvent(event);
+
+        return order;
+    }
+
+
+
+    public List<Order> fetchAllOrders() {
+        return orderRepo.findAll();
+    }
+
+    // Add these two methods inside OrderService class
+    public Order markOutForDelivery(Long orderId) {
+        Order order = orderRepo.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+
+        order.setStatus(Order.OrderStatus.OUT_FOR_DELIVERY);
+        order = orderRepo.save(order);
+
+        OrderEvent event = new OrderEvent(
+                order.getId(),
+                order.getCustomerUsername(),
+                order.getProductName(),
+                order.getPrice(),
+                order.getStatus(),
+                "Your order is Out for Delivery!"
+        );
+
+        kafkaProducer.sendNotificationEvent(event);
+        return order;
+    }
+
+    public Order markDelivered(Long orderId) {
+        Order order = orderRepo.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+
+        order.setStatus(Order.OrderStatus.DELIVERED);
+        order = orderRepo.save(order);
+
+        OrderEvent event = new OrderEvent(
+                order.getId(),
+                order.getCustomerUsername(),
+                order.getProductName(),
+                order.getPrice(),
+                order.getStatus(),
+                "Your order has been Delivered!"
+        );
+        kafkaProducer.sendNotificationEvent(event);
+        return order;
+    }
+
+}
---
+++ src/main/java/com/LiveNotificationDemo/service/UserService.java
@@ -0,0 +1,88 @@
+package com.LiveNotificationDemo.service;
+
+import com.LiveNotificationDemo.entity.UserPrincipal;
+import com.LiveNotificationDemo.entity.Users;
+import com.LiveNotificationDemo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
+
+    //user
+    public Users register(Users user){
+        if(userRepo.findByUsername(user.getUsername()).orElse(null) != null)
+            throw new RuntimeException("Username already exists!!");
+
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+
+    public Map<String, String> login(Users user){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        // authenticate is the unimplemented method of AuthenticationManager interface whose one of the implementations is done is ProviderManager class
+        // provider manager.authenticate() is called -> then it iterates through the available provider
+        // i am using DAO so its dynamically added to the list of providerManager
+//          now authentication is done by dao class's object
+//        in that method :the flow is
+//        1. load the user by username from database
+//        (method present in UserDetailsService) and i have implemented that on CustomUserDetailsService
+//        2. verifying password by encoding entered one to db's password
+//        3. returning an authenticated token with authorities
+
+
+        if (!authentication.isAuthenticated()) throw new RuntimeException("Invalid credentials");
+
+        Users dbUser = userRepo.findByUsername(user.getUsername()).orElse(null);
+
+        // Generate tokens
+        String accessToken = jwtService.generateAccessToken(new UserPrincipal(dbUser));
+        String refreshToken = jwtService.generateRefreshToken(new UserPrincipal(dbUser));
+
+        // Save refresh token in DB
+        dbUser.setRefreshToken(refreshToken);
+        userRepo.save(dbUser);
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("accessToken", accessToken);
+        tokens.put("refreshToken", refreshToken);
+        return tokens;
+    }
+
+    public Map<String, String> refresh(String refreshToken) {
+        Users user = userRepo.findByRefreshToken(refreshToken).orElse(null);
+        if (user == null || jwtService.validateToken(refreshToken, new UserPrincipal(user))) throw new RuntimeException("Invalid refresh token");
+
+        String newAccessToken = jwtService.generateAccessToken(new UserPrincipal(user));
+        String newRefreshToken = jwtService.generateRefreshToken(new UserPrincipal(user));
+
+        user.setRefreshToken(newRefreshToken);
+        userRepo.save(user);
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("accessToken", newAccessToken);
+        tokens.put("refreshToken", newRefreshToken);
+        return tokens;
+    }
+
+}
+
