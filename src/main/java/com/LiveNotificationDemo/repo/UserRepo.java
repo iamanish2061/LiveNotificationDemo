de955a9959a7b4c3a2e3143143032e0b2ad1b26b
AnishShrestha <anishnitro5@gmail.com>
2025-11-23 18:23:44 +0545

    building repository user and order

---
+++ src/main/java/com/LiveNotificationDemo/repo/UserRepo.java
@@ -0,0 +1,17 @@
+package com.LiveNotificationDemo.repo;
+
+import com.LiveNotificationDemo.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    Optional<Users> findByRefreshToken(String token);
+}
