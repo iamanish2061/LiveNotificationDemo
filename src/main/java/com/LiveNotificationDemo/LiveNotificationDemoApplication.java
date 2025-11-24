e42746f9646c14e489bb14100c58f40b78f3400f
AnishShrestha <anishnitro5@gmail.com>
2025-11-24 13:04:48 +0545

    removed bean

--- src/main/java/com/LiveNotificationDemo/LiveNotificationDemoApplication.java
+++ src/main/java/com/LiveNotificationDemo/LiveNotificationDemoApplication.java
@@ -2,10 +2,6 @@ package com.LiveNotificationDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @SpringBootApplication
 public class LiveNotificationDemoApplication {
@@ -13,6 +9,5 @@ public class LiveNotificationDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LiveNotificationDemoApplication.class, args);
 	}
-
 
 }
